cd6291bd7ee8ca7910d68c28383d13a277c5d528
JesusHF <jehernandez1996@gmail.com>
2016-11-03 10:32:27 +0100

    Revert "header"
    This reverts commit d60534a7b35398b7a0ae1168b06b8ad986142f59.

--- includes/headerL.php
+++ includes/headerL.php
@@ -2,12 +2,12 @@
 
 ?>
 
-<header>
+<header class="text">
   <a href="index.php" id="logo"><img src="images/logo.jpg" alt="logo" /></a>
 
   <form action="buscar.php" method="GET" class="search">
 
-    <label for="search">Buscar: </label><input id="search" class="box"  placeholder="Busqueda" name="busqueda" type="search"/>
+    <label for="search">Buscar: </label><input id="search"  placeholder="Busqueda" name="busqueda" type="search" autofocus/>
     <button type="submit"> <i class="material-icons">search</i></button>
 
   </form>
@@ -38,8 +38,8 @@
   <div class="loginF">
     <!--a class="boton" href="" id="butt">Entrar</a-->
     <form action="index.php" method="POST" class="">
-      <label for="user">Usuario: </label><input  id="user" class="box" name="user" type="text" placeholder="Usuario" required/>
-      <label for="pass">Contraseña: </label><input id="pass" class="box"  name="password" type="password" placeholder="Contraseña" required/>
+      <label for="user">Usuario: </label><input  id="user" name="user" type="text" placeholder="Usuario" required/>
+      <label for="pass">Contraseña: </label><input id="pass"  name="password" type="password" placeholder="Contraseña" required/>
       <label class="show" for="remember">¿recordarme?</label><input id="remember"type="checkbox" name="remember" >
       <button class="boton" type="submit">Entrar</button>
     </form>

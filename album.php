109df804ae12c1c68d2fe674fa3ebc68af0dc942
JesusHF <jehernandez1996@gmail.com>
2016-12-04 12:59:42 +0100

    album (fail)

--- album.php
+++ album.php
@@ -1,61 +1,65 @@
 <?php
-  $title= "Imagen";
-  include("includes/head.php");
+$title= "Imagen";
+include("includes/head.php");
 
-  if(isset($_SESSION["remember"])==false){
-		header("location: index.php");
-    exit;
+if(isset($_SESSION["remember"])==false){
+  header("location: index.php");
+  exit;
+}
+include("includes/headerC.php");
+
+
+if(isset($_GET)){
+  if(isset($_GET['id'])){
+    $id= $_GET['id'];
+
+    $sentencia="SELECT Titulo, Fecha, Usuario, NomPais FROM albumes, paises WHERE Pais=idPais AND idAlbum =$id";
+    $resultado= mysqli_query($bbdd, $sentencia);
   }
-  include("includes/headerC.php");
-
-
-  if(isset($_GET)){
-    if(isset($_GET['id'])){
-      $id= $_GET['id'];
-      $resultado= mysqli_query($bbdd, "SELECT Titulo, Fecha, Pais, Usuario FROM albumes WHERE idAlbum =".$id);
-
-      if($fila=$resultado->fetch_assoc()){
-        $titulo= $fila ["Titulo"];
-        $fecha= $fila ["Fecha"];
-        $pais= $fila ["Pais"];
-
-        if($pais!=null){
-          $resultadopais = mysqli_query($bbdd, "SELECT NomPais FROM paises WHERE idPais=".$pais);
-          $filapais= $resultadopais->fetch_assoc();
-          $nombrepais= $filapais['NomPais'];
-        }else{
-          $pais="Desconocido";
-        }
-
-        $resultadofoto= mysqli_query($bbdd, "SELECT Titulo, Fichero FROM fotos WHERE Album=".$id);
-      }
-
-    }
-  }
+}
 ?>
-  <main>
-    <?php
-      echo <<<HEREDOC
-        <h2 id='titulo'> $titulo</h2>
-        <h3 id='fecha'>Fecha: $fecha</h3>
-HEREDOC;
+<main>
+  <?php
+  if ($resultado!=false && $resultado->num_rows> 0){
+    if($fila=$resultado->fetch_assoc()){
+      $titulo= $fila ["Titulo"];
+      $fecha= $fila ["Fecha"];
+      $nombrepais= $fila["NomPais"];
+
+      $resultadofoto= mysqli_query($bbdd, "SELECT Titulo, Fichero, idFoto FROM fotos WHERE Album=".$id);
+
+
+      $html= <<<HTML
+<h2 id='titulo'> $titulo</h2>
+<h3 id='fecha'>Fecha: $fecha</h3>
+HTML;
+
+      echo $html;
+
       $nfotos=0;
       while($filafoto=$resultadofoto->fetch_assoc()){
-        $foto=$filafoto['Fichero'];
-        $titulo=$filafoto['Titulo'];
+        $fotof=$filafoto['Fichero'];
+        $titulof=$filafoto['Titulo'];
+        $idFoto= $filafoto['idFoto'];
 
-        echo "<figure id='detalleImg'><img width='70%' src='$foto' alt='$titulo'/></figure>";
+        $html2 = <<<HTML2
+\n<h3>$titulof</h3>
+<figure id='detalleImg'><a href="imagen.php?id=$idFoto"><img width='70%' src='$fotof' alt='$titulof'/></a></figure>
+HTML2;
+
+        echo $html2;
         $nfotos++;
       }
       if($nfotos===0){
         echo "<h1 class='index'>No hay fotos</h1>";
       }
-      echo "<h3>Pais: $nombrepais</h3>\n
-      <p>";
+      echo "\n<h3>Pais: $nombrepais</h3>\n";
+    }
+  }
+  else{
+      echo "<h1> Error 404: Album no encontrado</h1>\n";
+  }
+  ?>
+</main>
 
-
-      echo "</p>";
-    ?>
-  </main>
-
-  <?php include("includes/footer.php");?>
+<?php include("includes/footer.php");?>
